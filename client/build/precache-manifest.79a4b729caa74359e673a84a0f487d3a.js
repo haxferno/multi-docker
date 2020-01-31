2f8d8a3f6d8e85941112d76c98c9781bc6c4aa95
Inferno24 <inferno.pranjal24@gmail.com>
2020-01-31 18:30:32 +0530

    Final Commit

---
+++ client/build/precache-manifest.79a4b729caa74359e673a84a0f487d3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9833fb0edb32bec4316c8b7a5ed2604d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "473083952122ec52a67b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9456c5869bba87fdf487",
+    "url": "/static/js/2.42b99d59.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.42b99d59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "473083952122ec52a67b",
+    "url": "/static/js/main.000f761f.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ client/src/OtherPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return(
+        <div>
+            You are in a Multiverse!
+            <Link to="/">   Back to Earth</Link>
+        </div>
+    );
+};
+
+
+
+
+
